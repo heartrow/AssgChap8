fe760977e37d31ead227cd0f01ba5492072264cc
Amirul Hakim <amirulhazhar@gmail.com>
2026-05-19 12:03:15 +0800

    added password db

--- server/db.js
+++ server/db.js
@@ -1,12 +1,13 @@
 // server/db.js
+require('dotenv').config()
 const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'root', // Laragon default; do NOT do this in production
-    database: 'employee_records',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD, // Laragon default; do NOT do this in production
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
