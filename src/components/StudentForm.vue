834b1633b992f5f44a42edfd9b44f02a3f578223
Amirul Hakim <amirulhazhar@gmail.com>
2026-05-19 18:56:09 +0800

    StudentForm config

---
+++ src/components/StudentForm.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, watch, computed } from 'vue'
+
+const props = defineProps({
+    editingEmployees: { type: Object, default: null }
+})
+const emit = defineEmits(['save', 'cancel'])
+
+const emptyForm = () => ({
+    empId: '', name: '', email: '', department: '', 
+    position: '', hireDate: '', salary: '', active: true
+})
+
+const form = ref(emptyForm())
+const errors = ref({})
+
+watch(() => props.editingEmployee, (val) => {
+    form.value = val? { ...val } : emptyForm()
+    errors.value = {}
+}, { immediate: true })
+
+const isEditing = computed(() => Boolean(props.editingEmployee))
+</script>
